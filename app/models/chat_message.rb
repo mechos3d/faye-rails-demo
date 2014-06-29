728f9500180d1ecbdd13d695cfb42c23f17c4611
Keenan Brock <kbrock@redhat.com>
2014-06-29 19:33:44 -0400

    use ActiveRecord CRUD for ChatMessage
    - more efficient top 25
    - recent messages acts more like a named scope than "find"

--- app/models/chat_message.rb
+++ app/models/chat_message.rb
@@ -3,19 +3,29 @@ class ChatMessage
 
   attr_accessor :message, :created_at
 
-  def initialize(message, created_at=Time.now)
-    @message = message
-    @created_at = created_at
+  def initialize(options = {})
+    options.each_pair do |n, v|
+      public_send("#{n}=", v)
+    end
+    self.created_at ||= Time.now
+  end
+
+  def push
     self.class.push self
+    self
   end
 
   def self.push(chat_message)
     @chat_messages ||= []
     @chat_messages << chat_message
-    @chat_messages = @chat_messages.reverse.take(HISTORY).reverse
+    @chat_messages.shift if @chat_messages.size > HISTORY
   end
 
-  def self.find
+  def self.create(options)
+    new(options).tap { |cm| push cm }
+  end
+
+  def self.recent
     @chat_messages ||= []
   end
 end

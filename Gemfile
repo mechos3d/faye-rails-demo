458c25166b199cea9ab8c933f3a046274df8b718
Keenan Brock <kbrock@redhat.com>
2014-08-16 23:46:13 -0400

    Update to faye-rails 2.0
    - fix jquery differences

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 ruby '1.9.3'
 
 gem 'rails', '3.2.11'
-gem 'faye-rails', '~> 1.0.6'
+gem 'faye-rails', '~> 2.0'
 gem 'thin'
 gem 'haml-rails'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 FayeRailsDemo::Application.routes.draw do
-  faye_server '/faye', timeout: 25 do
-    map '/chat' => RealtimeChatController
-    map default: :block
-  end
-
-  resources :chat
   root to: 'chat#index'
 end
